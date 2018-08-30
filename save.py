49b3078990ad73ee55ced6b99286ab39b2d02187
Friedrich <fs@weareint.io>
2018-08-30 15:58:26 +0200

    console output improved

--- save.py
+++ save.py
@@ -1,19 +1,24 @@
+#!/bin/python
+
 # Saves a CSV file <accountname>.csv which contains the 
 # current date, posts, followers, following.
 
 # Usage: python <this-filename>.py <accountname>
 
+import os
 import sys
 import requests
 import csv
 import time
 
 from bs4 import BeautifulSoup
-from pyvirtualdisplay import Display 
+from pyvirtualdisplay import Display
 from selenium import webdriver
 
 account = sys.argv[1]
 url = 'https://www.instagram.com/' + account
+path = os.path.realpath(__file__)
+path = os.path.dirname(path)
 
 # set up chrome driver to read react-generated page
 
@@ -29,21 +34,45 @@ options.add_argument('--lang=en-US')
 service_log_path = "./chromedriver.log"
 service_args = ['--verbose']
 
-browser = webdriver.Chrome("./chromedriver",chrome_options=options, service_args=service_args, service_log_path=service_log_path)
-browser.implicitly_wait(25)
+print('Starting Chrome ...')
+browser = webdriver.Chrome(chrome_options=options, service_args=service_args, service_log_path=service_log_path)
+browser.set_page_load_timeout(60) 
+browser.implicitly_wait(25) 
 browser.get(url)
 
+print('')
+print('------')
+print('>>',url)
+print('------')
+print('')
+
+
 # get the data
+print('Starting to scrape the data from',account)
 soup = BeautifulSoup(browser.page_source,"html.parser")
 
-numbers = soup.find('header').find('section').find('ul').find_all('li')
+numbers = soup.select('header.vtbgv ul li')
 
 posts = numbers[0].find('span').find('span').contents[0]
 followers = numbers[1].find('span').contents[0]
 following = numbers[2].find('span').contents[0]
 
+
+# format numbers
+posts = posts.replace(',','')
+followers = followers.replace(',','')
+following = following.replace(',','')
+
 date = time.strftime("%Y-%m-%d")
 
+
 # write the data
-f = csv.writer(open(account+'.csv', 'a')) # open file in append mode
+
+print('')
+print('Writing', date, posts, followers, following, 'to', path+'/'+account+'.csv',)
+
+f = csv.writer(open(path+'/'+account+'.csv', 'a')) # open file in append mode
 f.writerow([date,posts,followers,following]) # write to the file
+
+# sleep a while for crontab & screen debugging
+# time.sleep(60)
